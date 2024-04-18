1b78e5121c8588af5fad342975380b7cfd528df8
someone <someone@someplace.com>
2024-04-19 03:27:44 +0330

    sample

---
+++ project/__main__.py
@@ -0,0 +1,7 @@
+# my_package/__main__.py
+
+# def default_behavior():
+#     print("This is the default behavior of my project")
+#
+# if __name__ == "__main__":
+#     default_behavior()
--- project/demo.py
+++ project/demo.py
@@ -1,65 +1,62 @@
 import os
 import sqlite3
-
+from datetime import datetime
+import json
 
 def create_db(db_name):
     if not os.path.exists(db_name):
-        connection = sqlite3.connect(db_name)
-        cursor = connection.cursor()
-        cursor.execute("""CREATE TABLE IF NOT EXISTS person(ID INTEGER PRIMARY KEY AUTOINCREMENT,first_name TEXT,
-        last_name TEXT, birthday INTEGER,weight REAL);""")
-        connection.commit()
-        connection.close()
-        print('table created')
-        return connection
+        with sqlite3.connect(db_name) as connection:
+            cursor = connection.cursor()
+            cursor.execute("""CREATE TABLE IF NOT EXISTS person(ID INTEGER PRIMARY KEY AUTOINCREMENT,first_name TEXT,
+                            last_name TEXT, birthday DATE,weight REAL);""")
+            print('Table created')
+    else:
+        print('Database exists.')
 
-    print('Database exist.')
-
-
-def insert_data(db_name):
+def insert_data(db_name, first_name, last_name, birthday, weight):
     if not os.path.exists(db_name):
         print("Database does not exist. Please create the database first.")
-        return None
-    else:
-        # Connect to your SQLite database
-        connection = sqlite3.connect(db_name)
+        return
 
+    with sqlite3.connect(db_name) as connection:
         cursor = connection.cursor()
-
-        insert_query = "INSERT INTO person (first_name, last_name,birthday,weight) VALUES (?, ?, ?, ?);"
-
-        firs_name = input('Firs Name: ')
-        last_name = input('Last Name: ')
-        birthday = int(input('Birthday: '))
-        weight = float(input('Weight: '))
-
-        values = (firs_name, last_name, birthday, weight)
-
+        insert_query = "INSERT INTO person (first_name, last_name, birthday, weight) VALUES (?, ?, ?, ?);"
+        values = (first_name, last_name, birthday, weight)
         cursor.execute(insert_query, values)
-        connection.commit()
-        cursor.close()
-        connection.close()
-        print('data insert')
 
+        print('Data inserted')
+
+    convert_to_json('last.db', 'user_data.json')
+def calculate_age(birthday_str):
+    birthday = datetime.strptime(birthday_str, '%Y-%m-%d').date()
+    today = datetime.today().date()
+    age = today.year - birthday.year - ((today.month, today.day) < (birthday.month, birthday.day))
+    return age
 
 def show_data(db_name):
     if not os.path.exists(db_name):
         print("Database does not exist. Please create the database first.")
         return None
 
-    # Connect to your SQLite database
-    conn = sqlite3.connect(db_name)
-    cursor = conn.cursor()
+    with sqlite3.connect(db_name) as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM person;")
+        tables = cursor.fetchall()
+        list_of_users_data = []
 
-    # Execute a query to get a list of tables
-    cursor.execute("SELECT * FROM person;")
+        for table in tables:
+            table = list(table)
+            table[3] = calculate_age(table[3])
+            list_of_users_data.append(table)
 
-    # Fetch all the results
-    tables = cursor.fetchall()
+    return list_of_users_data
 
-    # Print the list of tables
-    for table in tables:
-        print(table)
+def convert_to_json(db_file, json_file):
+    with open(json_file, 'w') as file:
+        for inner_list in show_data(db_file):
+            json_data = json.dumps(inner_list, ensure_ascii=False, indent=4)
+            file.write(json_data + '\n')
 
-    cursor.close()
-    conn.close()
+if __name__ == '__main__':
+    #for example
+    print('this is the main file.')
